3c257cd498b803ca374e8a3f2c08791c7851d389
Dmitry Petelin <petelin.dmitry@gmail.com>
2024-01-19 20:32:20 +0300

    refactor: use map function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 const folder = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folder).then((elem) => {
-  elem.forEach((el) => {
-    const name = el.slice(0, el.lastIndexOf('.'));
-    const ext = path.extname(el).slice(1);
-    fs.stat(path.join(__dirname, 'secret-folder', el)).then((e) => {
-      if (e.isFile()) {
-        console.log(name + ' - ' + ext + ' - ' + e.size / 1024 + 'kb');
+fs.readdir(folder).then((arr) => {
+  arr.map((elem) => {
+    fs.stat(path.join(__dirname, 'secret-folder', elem)).then((file) => {
+      if (file.isFile()) {
+        console.log(
+          `${elem.slice(0, elem.lastIndexOf('.'))} - ${path
+            .extname(elem)
+            .slice(1)} - ${file.size / 1024}kb`,
+        );
       }
     });
   });
